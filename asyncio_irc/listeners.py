8d70873abebd67dadb621c3ad43f388f82e9824f
Charlie Denton <charlie@meshy.co.uk>
2015-02-03 21:02:38 +0000

    Add WhitelistListener and BlacklistListener

--- asyncio_irc/listeners.py
+++ asyncio_irc/listeners.py
@@ -15,6 +15,29 @@ class CommandListener(Listener):
         if message.command == self.command:
             super().handle(connection, message)
 
+
+class WhitelistListener(Listener):
+    """Invokes the handler for a whitelist of commands."""
+    def __init__(self, whitelist, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.whitelist = [command.value for command in whitelist]
+
+    def handle(self, connection, message):
+        if message.command in self.whitelist:
+            super().handle(connection, message)
+
+
+class BlacklistListener(Listener):
+    """Invokes the handler for all but a blacklist of commands."""
+    def __init__(self, blacklist, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.blacklist = [command.value for command in blacklist]
+
+    def handle(self, connection, message):
+        if message.command not in self.blacklist:
+            super().handle(connection, message)
+
+
 # class RegexListener(Listener):
 #     def __init__(self, regex, *args, **kwargs):
 #         super().__init__(*args, **kwargs)

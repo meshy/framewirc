91ef4406746d7dc153a2edf0fc4c3019f018cb70
Charlie Denton <charlie@meshy.co.uk>
2015-02-04 15:29:13 +0000

    Simplify ping/pong

--- asyncio_irc/handlers.py
+++ asyncio_irc/handlers.py
@@ -3,7 +3,5 @@ from .commands import Command
 
 def ping(connection, message):
     """On recieving PING, repond with PONG."""
-    payload = Command.PONG.value
-    if message.trailing:
-        payload += b' :' + message.trailing
+    payload = Command.PONG.value + b' :' + message.trailing
     connection.send(payload)

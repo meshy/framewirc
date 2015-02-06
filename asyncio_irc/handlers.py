086b08a6521d5d325d60196e0981783a8713df9d
Charlie Denton <charlie@meshy.co.uk>
2015-02-06 23:17:23 +0000

    Ensure that `ping` only responds to PING

--- asyncio_irc/handlers.py
+++ asyncio_irc/handlers.py
@@ -1,6 +1,8 @@
+from . import commands, filters
 from .message import build_message
 
 
+@filters.command_only(commands.PING)
 def ping(connection, message):
     """On recieving PING, repond with PONG."""
     payload = build_message('PONG', suffix=message.suffix)
--- asyncio_irc/tests/test_handlers.py
+++ asyncio_irc/tests/test_handlers.py
@@ -13,3 +13,12 @@ class TestPing(TestCase):
         handlers.ping(connection, message)
 
         connection.send.assert_called_with(b'PONG :hostname.example.com\r\n')
+
+    def test_other(self):
+        """Do not respond to other commands."""
+        connection = mock.MagicMock()
+        message = ReceivedMessage(b'DERP :hostname.example.com')
+
+        handlers.ping(connection, message)
+
+        self.assertFalse(connection.send.called)

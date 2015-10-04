de035ecda6acc193731cff298967da9514e74b89
Charlie Denton <charlie@meshy.co.uk>
2015-10-04 01:06:34 +0100

    cChardet seems to work on version 1.0.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     description='An asynchronous IRC framework/library based upon asyncio',
     include_package_data=True,
     install_requires=[
-        'cchardet>=0.3.5,<0.4',
+        'cchardet>=0.3.5,<2',
     ],
     name='asyncio-irc',
     packages=['asyncio_irc'],

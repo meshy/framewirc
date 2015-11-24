2e0add5496e9b3db77613e11876452705bf1d98a
Charlie Denton <charlie@meshy.co.uk>
2015-11-24 21:01:23 +0000

    Convert test_client to pytest

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,7 @@
 import asyncio
-from unittest import mock, TestCase
+from unittest import mock
+
+import pytest
 
 from framewirc import exceptions
 from framewirc.client import Client
@@ -9,23 +11,23 @@ from framewirc.message import ReceivedMessage
 from .utils import BlankClient
 
 
-class TestConnectTo(TestCase):
+class TestConnectTo:
     def test_connection_stored(self):
         """Has "connection" been stored on the client?"""
         client = BlankClient()
         with mock.patch('asyncio.Task', spec=asyncio.Task):
             client.connect_to('irc.example.com')
-        self.assertIsInstance(client.connection, Connection)
+        assert isinstance(client.connection, Connection)
 
     def test_task_returned(self):
         """Is the correct "Task" created and returned?"""
         client = BlankClient()
         with mock.patch('asyncio.Task', spec=asyncio.Task) as Task:
             result = client.connect_to('irc.example.com')
-        self.assertEqual(result, Task(client.connection.connect()))
+        assert result == Task(client.connection.connect())
 
 
-class TestOnMessage(TestCase):
+class TestOnMessage:
     def test_handlers_called(self):
         """When a message comes in, it should be passed to the handlers."""
         handler = mock.MagicMock()
@@ -37,8 +39,8 @@ class TestOnMessage(TestCase):
         handler.assert_called_with(client, message)
 
 
-class TestOnConnect(TestCase):
-    def setUp(self):
+class TestOnConnect:
+    def setup_method(self, method):
         """Can't make an IRC connection in tests, so a mock will have to do."""
         self.client = Client(
             handlers=[],
@@ -58,7 +60,7 @@ class TestOnConnect(TestCase):
         self.client.connection.send.assert_called_with(expected)
 
 
-class TestPrivmsg(TestCase):
+class TestPrivmsg:
     def test_simple_message(self):
         client = BlankClient()
         client.connection = mock.MagicMock(spec=Connection)
@@ -80,26 +82,26 @@ class TestPrivmsg(TestCase):
         client.connection.send_batch.assert_called_once_with(expected)
 
 
-class TestRequiredFields(TestCase):
+class TestRequiredFields:
     """Test to show that RequiredAttribuesMixin is properly configured."""
 
     def test_fields(self):
         """Are the correct fields being checked?"""
-        required = ['handlers', 'real_name', 'nick']
-        self.assertCountEqual(Client.required_attributes, required)
+        required = ('handlers', 'real_name', 'nick')
+        assert Client.required_attributes == required
 
     def test_uses_required_attributes_mixin(self):
         """Is RequiredAttributesMixin.__init__ actually getting called?"""
-        with self.assertRaises(exceptions.MissingAttributes) as cm:
+        with pytest.raises(exceptions.MissingAttributes) as exception:
             Client()
 
-        expectied = "Required attribute(s) missing: ['handlers', 'real_name', 'nick']"
-        self.assertEqual(str(cm.exception), expectied)
+        expected = "Required attribute(s) missing: ['handlers', 'real_name', 'nick']"
+        assert expected in str(exception)
 
 
-class TestSetNick(TestCase):
+class TestSetNick:
     """Test the Client.set_nick() method."""
-    def setUp(self):
+    def setup_method(self, method):
         """Can't make an IRC connection in tests, so a mock will have to do."""
         self.client = BlankClient()
         self.client.connection = mock.MagicMock(spec=Connection)
@@ -114,4 +116,4 @@ class TestSetNick(TestCase):
         """Should store the new nick on the Client."""
         new_nick = 'meshy'
         self.client.set_nick(new_nick)
-        self.assertEqual(self.client.nick, new_nick)
+        assert self.client.nick == new_nick

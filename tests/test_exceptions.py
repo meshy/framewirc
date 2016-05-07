c86ccf75fdf49115697548bb046bc83f348a1aba
Charlie Denton <charlie@meshy.co.uk>
2016-05-07 19:02:24 +0100

    Fix test that wasn't running

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,11 +1,10 @@
 from framewirc import exceptions
 
 
-class MissingAttributesTest:
-    def test_message(self):
-        attrs = ['some', 'attrs']
-        expected = 'Required attribute(s) missing: {}'.format(attrs)
+def test_message():
+    attrs = ['some', 'attrs']
+    expected = 'Required attribute(s) missing: {}'.format(attrs)
 
-        exception = exceptions.MissingAttributes(attrs)
+    exception = exceptions.MissingAttributes(attrs)
 
-        assert str(exception) == expected
+    assert str(exception) == expected
